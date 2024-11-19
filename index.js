658a35fc2e4e987d1c46e6b7831bfe643f13a2c4
KALAISELVAN CK <posttokalai@gmail.com>
2024-11-19 16:21:14 +0530

    route for login added

--- index.js
+++ index.js
@@ -49,6 +49,31 @@ app.post("/register", async (req, res) => {
   }
 });
 
+// 2. User Login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const result = await pool.query("SELECT * FROM Users WHERE username = $1", [
+      username,
+    ]);
+    const user = result.rows[0];
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET
+    );
+    res.json({ token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
